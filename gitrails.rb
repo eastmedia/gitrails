fcbaa9fa7a19e2e8cf456e1b31a103c7336f5f35
Scott Taylor <scott@railsnewbie.com>
2008-06-18 12:16:21 -0400

    * Adding rspec, rspec_on_rails, rails to vendor as submodules * Adding execute() and execute_in_current_dir() for more flexible output

--- gitrails.rb
+++ gitrails.rb
@@ -15,26 +15,80 @@ if ARGV.size != 1
   exit
 end
 
-project = ARGV[0]
+def project
+  ARGV[0]
+end
+
+def execute_in_current_dir(cmd)
+  puts "** Executing #{cmd}"
+  puts `#{cmd}`
+end
+
+def execute(cmd)
+  cd = File.exists?(project) ? "cd #{project};" : ""
+  puts "** Executing #{cmd}"
+  puts `#{cd} #{cmd}`
+end
 
 puts "Creating rails application: #{project}"
-`rails #{project}`
+execute "rails #{project}"
 
 puts "Removing log files"
-`cd #{project}; rm -rf log/*`
-`cd #{project}; touch .gitignore; echo "log/*.log" >> .gitignore`
-`cd #{project}; touch log/.gitignore`
+execute "rm -rf log/*"
+execute "touch .gitignore; echo 'log/*.log' >> .gitignore"
+execute "touch log/.gitignore"
 
 puts "Removing tmp files"
-`cd #{project}; echo "tmp/**/*" >> .gitignore`
+execute "echo 'tmp/**/*' >> .gitignore"
 
 puts "Moving database.yml to database.example.yml"
-`cd #{project}; mv config/database.yml config/database.example.yml`
-`cd #{project}; echo "config/database.yml" >> .gitignore`
+execute "mv config/database.yml config/database.example.yml"
+execute "echo 'config/database.yml' >> .gitignore"
 
 puts "Importing application into repository"
-`cd #{project}; git init`
-`cd #{project}; git add .`
-`cd #{project}; git commit -m "initial import"`
+execute "git init"
+execute "git add ."
+execute "git commit -m 'initial import'"
 
-exit
+
+########################
+# Plugins 
+#######################
+
+puts "* Checkout out plugins (rspec, rspec_on_rails - it might take a while)"
+execute "git submodule add git://github.com/dchelimsky/rspec.git          vendor/plugins/rspec"
+execute "git submodule add git://github.com/dchelimsky/rspec-rails.git    vendor/plugins/rspec_on_rails"
+execute "git add ."
+execute "git commit -m '* Adding rspec, rspec on rails a submodule'"
+
+puts "* Moving rspec and rspec on rails to 1.1.4 release"
+execute_in_current_dir "cd #{project}/vendor/plugins/rspec;          git checkout 1.1.4"
+execute_in_current_dir "cd #{project}/vendor/plugins/rspec_on_rails; git checkout 1.1.4"
+execute "git add vendor/plugins/rspec"
+execute "git add vendor/plugins/rspec_on_rails"
+execute "git commit -m '* Moving rspec and rspec on rails to version 1.1.4'"
+
+########################
+# Rails
+#######################
+puts "* Checkout out rails in vendor/rails (this might take a while)"
+execute "git submodule add git://github.com/rails/rails.git vendor/rails"
+execute "git add ."
+execute "git commit -m '* Adding vendor/rails a submodule'"
+
+puts "* Moving rails to version 2.1 tag"
+execute_in_current_dir "cd #{project}/vendor/rails; git checkout v2.1.0"
+execute "git add ."
+execute "git commit -m '* Moving rails to version 2.1'"
+
+
+
+
+# We need the database setup to run these:
+#
+# puts "* Generating rspec configs"
+# execute "./script/generate rspec"
+# execute "git add script/spec spec"
+# execute "git commit -m '* Generating configs for rspec'"
+
+
